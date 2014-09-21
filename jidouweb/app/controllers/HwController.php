67d9a3c551e4205fe75cb3a9cd99d68a61a74db9
Ubuntu <ubuntu@ip-10-45-15-111.ec2.internal>
2014-09-21 01:32:58 +0000

    Upload on dispenseDone

--- jidouweb/app/controllers/HwController.php
+++ jidouweb/app/controllers/HwController.php
@@ -14,9 +14,9 @@ class HwController extends \BaseController {
 
   public function dispenseDone() {
     $terminal_id = (isset($_GET['id'])?intval($_GET['id']):0);
-    $quantity = (isset($_GET['p'])?floatval($_GET['p']):0);
-    if ($terminal_id > 0 && $quantity > 0) {
-      error_log('Receieved ADD_CREDIT value ' . $quantity . ' event from terminal ' . $terminal_id);
+    $product = (isset($_GET['p'])?floatval($_GET['p']):0);
+    if ($terminal_id > 0 && $product > 0) {
+      error_log('Receieved DISPENSE_DONE value ' . $product . ' event from terminal ' . $terminal_id);
     } else {
       error_log('method called without params.');
     }
